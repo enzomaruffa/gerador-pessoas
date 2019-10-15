7385d1c6ba9abba4e3f93884de1aa640e492b17a
Caíque Coelho <caiquedpfc@gmail.com>
2019-10-15 00:53:08 -0300

    added cellphone and telephone random data generator

--- generator.py
+++ generator.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
 
 import random
 import hashlib
 import copy
+from random import randint
+from random import choice
 
 possible_names = ['Carolina', 'Guilherme', 'Daniel', 'Luna', 'Natalie', 'Elaini', 'Eduarda', 'Isabella', 'Júlia', 'André', 'Thiago', 'Sandra', 'Edson', 'Ossian', 'Laura', 'Dante', 'Fernando', 'Antonio', 'Denise', 'Eloisa', 'Eduardo', 'Sílvio', 'Pedro', 'José', 'Jorge', 'Alfredo', 'Amadeus', 'Marcos', 'Enzo', 'Mariana', 'Aline', 'Marcus', 'Silvana', 'Ada', 'Renan', 'Roberto', 'Caio', 'Maria', 'Livia', 'Emerson', 'Luiz', 'Luis', 'Alan']
 possible_surnames = ['Andrade', 'Krum', 'Kawasaki', 'Gallice', 'Pedri', 'Harres', 'Queiroz', 'Rodrigues', 'Vicente', 'Kniss', 'Yamada', 'Cremonezi', 'Schumacher', 'Campos', 'Kovalski', 'Strozzi', 'Iboshi', 'Handa', 'Megumi', 'Shinohata', 'Padovani', 'Olini', 'Martins', 'Pereira', 'Moreira', 'Lopes', 'Souza', 'Sousa', 'Gomes', 'Ameixa', 'Pera', 'Maruffa', 'Krajuska', 'Dudeque', 'Starosta', 'Franco', 'Dencker', 'Gil', 'Barreto', 'Kruger', 'Calopsita', 'Meneghel']
@@ -12,6 +16,7 @@ possible_street_prefixes = ['Marechal', 'Presidente', 'Coronel']
 possible_street_separator = [' de ', ' da ', ' ', ' ', ' ']
 possible_states = ['Amapá', 'Paraná', 'Amazonas', 'Rio Grande do Sul', 'Santa Catarina', 'São Paulo', 'Rio de Janeiro', 'Espírito Santo', 'Alagoas', 'Sergipe', 'Bahia', 'Mato Grosso', 'Mato Grosso do Sul', 'Pará', 'Minas Gerais']
 possible_cities = ['Curitiba', 'Manaus', 'Porto Alegre', 'Florianópolis', 'São Paulo', 'Rio de Janeiro', 'Vitória', 'Maceió', 'Aracaju', 'Salvador', 'Cuiabá', 'Campo Grande', 'Belém', 'Belo Horizonte', 'Palotina', 'Divinópolis', 'Nova Serrana', 'Monte Alegre do Sul', 'Santa Maria', 'Porto Seguro', 'Campo Largo', 'Curitibanos', 'Itapoá']
+possible_ddds = [67, 66, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 24, 27, 28, 31, 32, 33, 34, 35, 37, 38, 41]
 
 visaPrefixList = [
         ['4', '5', '3', '9'],
@@ -37,6 +42,8 @@ class Person:
 
 		self.cpf = self.generate_random_cpf()
 		self.street, self.number, self.complement, self.city, self.cep = self.generate_random_address()
+		self.telephone = self.generate_random_phone(True)
+		self.cellphone = self.generate_random_phone(False)
 		self.cardNickname, self.pan, self.expDate, self.cvv = self.generate_random_card()
 
 	def generate_random_name(self):
@@ -51,7 +58,7 @@ class Person:
 
 	def generate_random_password(self):
 		h = hashlib.md5()
-		h.update((self.name + self.surname).encode('utf-8'))
+		h.update((self.name + self.surname))
 		return (h.hexdigest())[0:6]
 
 	def generate_random_cpf(self):
@@ -60,6 +67,17 @@ class Person:
 			digit = random.randrange(0, 9)
 			cpf += str(digit)
 		return cpf
+
+	def generate_random_phone(self, telephone=True):
+	    if telephone:
+	        typePhone = ' '
+	    else:
+	        typePhone = ' 9'
+	    number = '+55 {0}{1}'.format(random.choice(possible_ddds), typePhone)
+	    number += str(randint(1,9))
+	    for n in range(7):
+	        number += str(randint(0,9))
+	    return number
 
 	def generate_random_address(self):
 		def generate_random_street():
@@ -166,15 +184,10 @@ for i in range(0, 10000):
 	people_list.append(person)
 
 file = open('pessoas.csv', 'w+')
-file.write('email,senha,nome,sobrenome,cep,endereco,numero,complemento,cidade,apelido cartao,pan,dataexp,cvv,cpf\n')
+file.write('email,senha,nome,sobrenome,cep,endereco,numero,complemento,cidade, telefone fixo, celular, apelido cartao,pan,dataexp,cvv,cpf\n')
 
 for person in people_list:
-	file.write(person.email + ',' + person.password + ',' + person.name + ',' + person.surname + ',' + person.cep + ',' + person.street + ',' + person.number + ',' + person.complement + ',' + person.city + ',' + person.cardNickname + ',' + person.pan + ',' + person.expDate + ',' + person.cvv + ',' + person.cpf + '\n')
-	print(person.name + ' ' + person.surname + ' / ' + person.email + ' / ' + person.password + ' / ' + person.cpf + ' / ' + person.street + ', ' + person.number + ', ' + person.complement + '. ' + person.city + ', ' + person.cep + ' / ' + person.cardNickname + ', ' + person.pan + ', ' + person.expDate + ', ' + person.cvv)
+	file.write(person.email + ',' + person.password + ',' + person.name + ',' + person.surname + ',' + person.cep + ',' + person.street + ',' + person.number + ',' + person.complement + ',' + person.city + ',' + person.telephone + ',' + person.cellphone + ',' + person.cardNickname + ',' + person.pan + ',' + person.expDate + ',' + person.cvv + ',' + person.cpf + '\n')
+	print(person.name + ' ' + person.surname + ' / ' + person.email + ' / ' + person.password + ' / ' + person.cpf + ' / ' + person.street + ', ' + person.number + ', ' + person.complement + '. ' + person.city + ', '  + person.telephone + ', '  + person.cellphone + ', ' + person.cep + ' / ' + person.cardNickname + ', ' + person.pan + ', ' + person.expDate + ', ' + person.cvv)
 
 file.close()
-
-
-
-
-
